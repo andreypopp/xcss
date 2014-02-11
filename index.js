6d67726e8ef8c94ac28d8ad9c0b6cf8c3ced1ec9
Andrey Popp <8mayday@gmail.com>
2014-02-11 19:26:36 +0400

    Handle .css files, fixes #4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 var fs          = require('fs');
 var compile     = require('./compiler');
 
-require.extensions['.xcss'] = function(module, filename) {
+require.extensions['.xcss'] = require.extensions['.css'] = function(module, filename) {
   var src = fs.readFileSync(filename, 'utf8');
   var options = {xcssModulePath: __filename};
   var compiled = compile(src, options);
